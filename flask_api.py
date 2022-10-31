64ce6e6212f1847963d15d40609e57894fd2add4
Andrey <78901996@mail.ru>
2022-10-31 06:06:09 +0400

    Removed useless returns

--- flask_api.py
+++ flask_api.py
@@ -13,14 +13,12 @@ def index():
 def first_button():
     users_hotspots = merge_datasets(*data_download())[0]
     print(count_users_hotspots(users_hotspots).head(10))
-    return "Nothing"
 
 
 @app.route("/button2")
 def second_button():
     users_hotspots = merge_datasets(*data_download())[0]
     print(count_users_hotspots_geo(users_hotspots).head(10))
-    return "Nothing"
 
 
 @app.route("/button3")
@@ -28,14 +26,12 @@ def third_button():
     users_hotspots = merge_datasets(*data_download())[0]
     for i in range(3):
         print(count_users_hotspots_over_time(users_hotspots)[i].head(10))
-    return "Nothing"
 
 
 @app.route("/button4")
 def fourth_button():
     users_hotspots = merge_datasets(*data_download())[0]
     print(count_users_hotspots_score(users_hotspots).head(10))
-    return "Nothing"
 
 
 @app.route("/button5")
@@ -43,7 +39,6 @@ def fifth_button():
     users_hotspots, users_conns = merge_datasets(*data_download())
     for i in range(5):
         print(count_users_unique_hotspots(users_conns, users_hotspots)[i].head(10))
-    return "Nothing"
 
 
 if __name__ == "__main__":
